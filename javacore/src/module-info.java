06500df73c3f1bf882fade07d69626212914f3e6
yuvar <yuvar@LAPTOP-KP9DKG5O>
2024-06-27 11:04:18 +0530

    this is my java project

---
+++ javacore/src/module-info.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class sample {
+	
+	publlic static void main(String[] args {
+		System.out.println("WELCOME TO JFS");
+		System.out.println("MVIT");
+		System.out.print("TNSIF");
+	}
+}
